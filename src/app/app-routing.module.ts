59a1b7353ed25cb6714f9b2f7e0ab188cff1383e
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-28 02:27:58 -0400

    ADD: Preload Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: 'src/app/pages/sign-in/sign-in.module#SignInModule'
+      },
+      {
+        path: 'dash',
+        loadChildren: 'src/app/components/sidebar/sidebar.module#SidebarModule'
+      }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
