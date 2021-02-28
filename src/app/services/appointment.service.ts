f30b15c8a6cc5b14b636e89d93a562ba9aca36e1
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-28 15:00:27 -0400

    ADD: Appointment Service

---
+++ src/app/services/appointment.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentService {
+
+  url: string = `${environment.api}appointments`;
+
+  constructor(private _http: HttpClient) { }
+
+  fetchAppointments = () => { return this._http.get(`${this.url}`).toPromise() }
+  
+  addAppointment = (appointment) => { return this._http.post(`${this.url}`, appointment).toPromise() }
+  editAppointment = (id, appointment) => { return this._http.put(`${this.url}/${id}`, appointment).toPromise() }
+
+}
