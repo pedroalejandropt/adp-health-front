c349b373eb252ff6e05d5f52884cf413a9dd52c9
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-28 02:56:25 -0400

    ADD: Use User Service

--- src/app/pages/pacients/pacients.component.ts
+++ src/app/pages/pacients/pacients.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-pacients',
@@ -11,21 +12,27 @@ export class PacientsComponent implements OnInit {
   header: any = {'actions': 'Actions', 'firstName': 'First Name', 'firstLastName': 'Last Name', 'email': 'Mail'}
 
   pacients: any[] = [
-    {firstName: 'Pedro', firstLastName: 'Pacheco', email: 'pp@gmail.com'},
-    {firstName: 'Arturo', firstLastName: 'Pacheco', email: 'aa@gmail.com'}
+    /* {firstName: 'Pedro', firstLastName: 'Pacheco', email: 'pp@gmail.com'},
+    {firstName: 'Arturo', firstLastName: 'Pacheco', email: 'aa@gmail.com'} */
   ];
 
   record: any = null;
 
   actions: Action[] = []
 
-  constructor() { }
+  loading: boolean = true;
+
+  constructor(private _service: UserService) { }
 
   ngOnInit() {
     this.actions = [
       { icon: 'menu', action: (icon) => this.hola(icon) },
       { icon: 'face', action: () => this.chao() }]
+
+      this._service.fetchUser().then((res: any) => { this.pacients = res; this.loading = false })
   }
+
+  
 
   hola = (icon) => this.record = icon;
   chao = () => console.log('chao');
