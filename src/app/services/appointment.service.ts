deb8cad13bd36186f007808b646bf106c940acdb
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-28 16:32:38 -0400

    ADD: Appointment By User Function

--- src/app/services/appointment.service.ts
+++ src/app/services/appointment.service.ts
@@ -12,6 +12,7 @@ export class AppointmentService {
   constructor(private _http: HttpClient) { }
 
   fetchAppointments = () => { return this._http.get(`${this.url}`).toPromise() }
+  fetchAppointmentsByUser = (id) => { return this._http.get(`${this.url}/active/${id}`).toPromise() }
   
   addAppointment = (appointment) => { return this._http.post(`${this.url}`, appointment).toPromise() }
   editAppointment = (id, appointment) => { return this._http.put(`${this.url}/${id}`, appointment).toPromise() }
