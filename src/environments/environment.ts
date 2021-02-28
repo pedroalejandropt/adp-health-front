b7f164ffc94b611b72112ec760023415ff0537f9
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-28 02:38:57 -0400

    ADD: Local Url as Enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { Url } from "src/assets/config/config";
+
 export const environment = {
-  production: false
+  production: false,
+  api: Url.local 
 };
 
 /*
