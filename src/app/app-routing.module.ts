88dcdd2a9fb1b196832b5d3aaedc19c8c55831dd
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-03-01 00:37:36 -0400

    ADD: Automatic Redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,9 @@ const routes: Routes = [
       {
         path: 'dash',
         loadChildren: 'src/app/components/sidebar/sidebar.module#SidebarModule'
+      },
+      { 
+        path: '**', redirectTo: '' 
       }
     ]
   }
