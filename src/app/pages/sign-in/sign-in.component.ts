1d4d51d5d3485b414c0c62f36cf813a54c5997ee
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-03-01 00:34:52 -0400

    UPDATE: Validations && Errors Messages

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { popUpMessage } from 'src/assets/alert/alert';
 
 @Component({
   selector: 'app-sign-in',
@@ -10,6 +11,7 @@ import { UserService } from 'src/app/services/user.service';
 export class SignInComponent implements OnInit {
 
   hide: boolean = true;
+  error: boolean = false;
   user: any = { email: '', password: '' };
 
   constructor(
@@ -21,10 +23,12 @@ export class SignInComponent implements OnInit {
   }
 
   login = () => 
-    this._service.signin(this.user).then((res)=> {
-      localStorage.setItem('user', JSON.stringify(res[0]));
-      this._router.navigate(['/dash'])
-    }, error => console.log('BUUU'));
+    this._service.signin(this.user).then(async (res)=> {
+      this.error = false;
+      await popUpMessage.fire({ icon: 'success', title: 'Signed in successfully' });
+      await localStorage.setItem('user', JSON.stringify(res[0]));
+      await this._router.navigate(['/dash'])
+    }, error => { popUpMessage.fire({ icon: 'error', title: 'Error trying to sign in.' }); this.error = true });
 
     checkUser = () => {
       if ( this.user.email=='' || this.user.password=='') return true; else return false;
