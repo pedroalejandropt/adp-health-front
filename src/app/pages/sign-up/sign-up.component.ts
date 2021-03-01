7525ca3eaa12deaffdd094dca2aad21303d8a8bf
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-03-01 00:34:20 -0400

    UPDATE: Validations && Error Messages

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { popUpMessage } from 'src/assets/alert/alert';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,6 +10,7 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class SignUpComponent implements OnInit {
 
+  error: string = '';
   hide: boolean = true;
   user: any = { firstName: '', middleName: '', firstLastName: '', secondLastName: '', email: '', password: '', role: 2 };
   
@@ -20,7 +22,12 @@ export class SignUpComponent implements OnInit {
   ngOnInit() {}
 
   signUp = () => {
-    this._service.signup(this.user).then(() => { console.log('YESS'); this._router.navigate(['/']); })
+    this._service.signup(this.user).then(async () => { 
+      this.error = '';
+      await popUpMessage.fire({ icon: 'success', title: 'Account Created Successfully' }); 
+      await this._router.navigate(['/']); 
+    },
+      error => { this.error = error.error.message; popUpMessage.fire({ icon: 'error', title: 'Error Creating an Account' });  }) 
   }
 
   checkUser = () => {
